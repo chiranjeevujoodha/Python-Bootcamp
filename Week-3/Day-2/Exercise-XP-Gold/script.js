d2f07b77f5efb5063faae144d99103a116ca8d63
Chiranjeev Ujoodha <chiranjeev.ujoodha@VLH.RG>
2023-11-07 13:49:05 +0400

    exercises

--- Week-3/Day-2/Exercise-XP-Gold/script.js
+++ Week-3/Day-2/Exercise-XP-Gold/script.js
@@ -125,3 +125,64 @@ console.log(1 == "1")
 console.log(1 === "1")  
 // Prediction: undefined
 // Actual: false
+
+
+//-------------------------------------------------
+
+//Exercise 6
+
+console.log(5 + "34")
+// Prediction:534
+// Actual:534
+
+console.log(5 - "4")
+// Prediction:NaN
+// Actual:1
+
+console.log(10 % 5)
+// Prediction:0
+// Actual:0
+
+console.log(5 % 10)
+// Prediction:5
+// Actual:5
+
+console.log("Java" + "Script")
+// Prediction:JavaScript
+// Actual:JavaScript
+
+console.log(" " + " ")
+// Prediction:  
+// Actual:  
+
+console.log(" " + 0)
+// Prediction: 0
+// Actual: 0
+
+console.log(true + true)
+// Prediction:true
+// Actual:2
+
+console.log(true + false)
+// Prediction:1
+// Actual:1
+
+console.log(false + true)
+// Prediction:1
+// Actual:1
+
+console.log(false - true)
+// Prediction:-1
+// Actual:-1
+
+console.log(!true)
+// Prediction:false
+// Actual:false
+
+console.log(3 - 4)
+// Prediction:-1
+// Actual:
+
+console.log("Bob" - "bill")
+// Prediction:NaN
+// Actual:NaN
