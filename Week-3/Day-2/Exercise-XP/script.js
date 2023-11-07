f623b9d7a971b7ba67dc1d383b7e3870d33eaeb3
Chiranjeev Ujoodha <chiranjeev.ujoodha@VLH.RG>
2023-11-07 14:24:02 +0400

    exercises

---
+++ Week-3/Day-2/Exercise-XP/script.js
@@ -0,0 +1,12 @@
+//Exercise 1 - Favorite color
+
+let sentence = ["my","favorite","color","is","blue"];
+
+console.log(sentence[0] + ' ' + sentence[1] + ' ' + sentence[2] + ' ' + sentence[3] + ' ' + sentence[4])
+
+//--------------------------------------------
+
+//Exercise 2 - Mixup
+
+let str1 = "mix";
+let str2 = "pod";
