5059679fc9174ce53562fb824a4f8cd485853b3a
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2023-11-15 16:49:31 +0400

    exercises

--- Week-4/Day-2/Exercise-XP/Exercise-2/script.js
+++ Week-4/Day-2/Exercise-XP/Exercise-2/script.js
@@ -3,12 +3,19 @@ const prompt = require('prompt-sync')()
 
 function calculateTip(){
     let amount = parseInt(prompt('Enter bill amount: '))
-    let tip = 0
-    let final_bill = 0
+    
+    
     if (amount < 50){
         tip = 0.2 * amount
-        final_bill = amount + tip
+        
+    } else if (amount >= 50 & amount <= 200) {
+        tip = 0.15 * amount
+    } else if (amount > 200){
+        tip = 0.1 * amount
     }
+
+    final_bill = tip + amount
+
     console.log('Tip amount: ' + tip, ',final bill: ' + final_bill)
 }
 
