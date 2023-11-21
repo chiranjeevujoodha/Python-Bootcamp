b126e0517d8be495cdec0f269f10fc27af67674b
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2023-11-21 15:33:50 +0400

    exercises

---
+++ Week-4/Day-5-Mini-Project/Calculator/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+function number(num){
+    console.log(num)
+}
+
+function operator(operator){
+    x = 10
+    y = 2
+    console.log(eval(x + operator + y))
+}
+
+function equal(){
+    pass
+}
+
--- Week-4/Day-5-Mini-Project/Play-The-Game/script.js
+++ Week-4/Day-5-Mini-Project/Play-The-Game/script.js
@@ -1,50 +1,75 @@
-// const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')()
 
 let enter_number;
 let computerNumber;
+
+guesses = 0
 
 
 function compareNumbers(userNumber, computerNumber){
 
     if (userNumber === computerNumber) {
-        alert('winner')
-    } else if (userNumber > computerNumber){
-        alert('Number too big, guess again!')
-        userNumber = prompt('Enter new number')
-        console.log(userNumber)
-        console.log(computerNumber)
+        console.log('winner')
+        guesses+= 1
+    } else if (userNumber > computerNumber) {
+        guesses += 1
+        console.log('number big, guess again');
+        var new_number = parseInt( prompt('enter new number: '));
+        console.log(new_number);
+        if (new_number === computerNumber) {
+            guesses += 1
+            console.log('guesses ' + guesses)
+            console.log('winner');
+        } else if (new_number > computerNumber) {
+            console.log('number big, guess again');
+        }
+    } else if (userNumber < computerNumber) {
+        console.log('number small, guess again');
+        var new_number = parseInt( prompt('enter new number: '))
+        console.log(new_number)
+        if (new_number === computerNumber) {
+            console.log('winner');
+        }      
     }
 }
 
-function yes_play(){
-
-    enter_number = parseInt( prompt('Enter a number: '))
-
-    if (isNaN(enter_number)){
-        alert('Sorry Not a number, Goodbye')
-    } else if (enter_number < 0 || enter_number > 10) {
-        alert('Sorry it’s not a good number, Goodbye')
-    } else {
-        // computerNumber = Math.floor(Math.random() * 10)
-        computerNumber = 2
+compareNumbers(10,4)
 
 
-        compareNumbers(enter_number, computerNumber)
+
+// function yes_play(){
+
+//     enter_number = parseInt( prompt('Enter a number: '))
+
+//     if (isNaN(enter_number)){
+//         alert('Sorry Not a number, Goodbye')
+//     } else if (enter_number < 0 || enter_number > 10) {
+//         alert('Sorry it’s not a good number, Goodbye')
+//     } else {
+//         // computerNumber = Math.floor(Math.random() * 10)
+//         computerNumber = 2
         
-    }
-
-}
 
 
+//         // compareNumbers(enter_number, computerNumber)
+        
+//     }
+//     console.log('computer: ' + computerNumber)
+//     console.log('chosen: ' + enter_number)
 
-function playTheGame(){
-    var play = confirm('Would you like to play the game? ')
-    if (play == false){
-        alert("No problem, Goodbye")
-    } else {
-        yes_play()
-    }
-}
+// }
+
+
+
+// function playTheGame(){
+//     var play = confirm('Would you like to play the game? ')
+//     if (play == false){
+//         alert("No problem, Goodbye")
+//     } else {
+//         yes_play()
+//     }
+// }
+
 
 
 
